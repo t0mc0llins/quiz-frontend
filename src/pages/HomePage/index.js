42f36be8c74a2a6d4779e0f2ab7ef29e95eed3ca
Alina Kovaleva <calipco.xp@gmail.com>
2022-05-02 13:17:48 +0200

    Worked on "Home Page"
    - created store folder "homePage"
    - created config folder to store all URLs
    - created "Home Page" as pages component

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,11 @@
+import { ImageList, ImageListItem } from "@mui/material";
+
+export default function HomePage() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to Movie Quiz</h1>
+      </div>
+    </>
+  );
+}
