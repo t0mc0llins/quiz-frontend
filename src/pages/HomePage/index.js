a48525cc64b4c6361ee47b3ba78afe23e52eda0b
Alina Kovaleva <calipco.xp@gmail.com>
2022-05-02 15:58:35 +0200

    Worked on "Home Page"
        - added button
        - working on styles

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllMovies } from "../../store/homePage/actions";
 import { selectMovies } from "../../store/homePage/selectors";
-import { ImageList, ImageListItem, Container } from "@mui/material";
+import { ImageList, ImageListItem, Container, Button } from "@mui/material";
 import { apiUrlPoster } from "../../config/constants";
+import "./style.css";
 
 export default function HomePage() {
   const dispatch = useDispatch();
@@ -16,20 +17,19 @@ export default function HomePage() {
   }, [dispatch]);
   return (
     <>
-      <Container style={{ width: "100%" }}>
-        {/* <h1>Welcome to Movie Quiz</h1> */}
-        <ImageList variant="quilted" cols={4} rowHeight={195}>
-          {listOfMovies.map((item) => (
-            <ImageListItem key={item.id}>
-              <img
-                alt={item.title}
-                loading="lazy"
-                src={`${apiUrlPoster}${item.backdrop_path}`}
-              />
-            </ImageListItem>
-          ))}
-        </ImageList>
-      </Container>
+      {/* <h1>Welcome to Movie Quiz</h1> */}
+      <ImageList variant="quilted" cols={4} rowHeight={195}>
+        {listOfMovies.map((item) => (
+          <ImageListItem key={item.id}>
+            <img
+              alt={item.title}
+              loading="lazy"
+              src={`${apiUrlPoster}${item.backdrop_path}`}
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+      <Button className="home-page-button">Start Game</Button>
     </>
   );
 }
