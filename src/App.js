4cfa44c0bc0ec641de897dd159150be637045669
poojadalai <poojadalai@gmail.com>
2022-05-02 14:28:59 +0200

    styling question page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import GamePage from "./pages/GamePage";
+import QuestionPage from "./pages/QuestionPage";
 
 function App() {
   return (
     <div className="App">
-    
       <Routes>
-      <Route path="/game" element={ <GamePage />} />
+        <Route path="/game" element={<GamePage />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="/question" element={<QuestionPage />} />
       </Routes>
     </div>
   );
