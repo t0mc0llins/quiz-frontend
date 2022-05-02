54383ceb7b9c6c08d246b1c43fc9c5697a32f3e8
Vasileios1314 <vasilis.bobonis@hotmail.com>
2022-05-02 13:18:06 +0200

    adding wheel

---
+++ src/pages/GamePage/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SpinningWheel from "../../components/SpinningWheel";
+
+export default function GamePage() {
+  return (
+    <div>
+      <SpinningWheel />
+    </div>
+  );
+}
