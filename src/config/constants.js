fed2ecbb0ebfb7eed738ba2f0eb1ed73fb21fdae
t0mc0llins <tomcollins93@gmail.com>
2022-05-02 13:18:28 +0200

    added question redux store

---
+++ src/config/constants.js
@@ -0,0 +1,1 @@
+export const apiKey = "2ae2c29452effa24ebcb3eb4ece182e0";
---
+++ src/store/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import question from "./question/reducer";
+
+export default combineReducers({
+  question,
+});
