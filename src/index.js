a2773d8029ec8d194d8722015a3e2f7b8c8ae393
t0mc0llins <99662880+t0mc0llins@users.noreply.github.com>
2022-05-02 13:24:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import store from "./store";
 import { Provider } from "react-redux";
+import store from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Router>,
-  document.getElementById("root")
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
