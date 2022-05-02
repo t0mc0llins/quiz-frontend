e45e824ce8597cd935732bd8f42fd7a1331755f4
poojadalai <poojadalai@gmail.com>
2022-05-02 16:05:35 +0200

    added buttons to the question page

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Typography } from "@mui/material";
+import React from "react";
 
 export default function Question() {
   return (
-    <div>Question</div>
-  )
+    <Typography
+      sx={{ fontFamily: `'Happy Monkey', cursive` }}
+      variant="h3"
+      gutterBottom
+      component="div"
+    >
+      What was the first movie in the Marvel Cinematic Universe?
+    </Typography>
+  );
 }
