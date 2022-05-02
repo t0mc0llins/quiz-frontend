b2086fbedb62d72af5537dc9cb75a6dad1dfbca6
Alina Kovaleva <calipco.xp@gmail.com>
2022-05-02 15:18:20 +0200

    Worked on "Home Page"
        - added collage of movies posters

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllMovies } from "../../store/homePage/actions";
 import { selectMovies } from "../../store/homePage/selectors";
-import { ImageList, ImageListItem } from "@mui/material";
+import { ImageList, ImageListItem, Container } from "@mui/material";
+import { apiUrlPoster } from "../../config/constants";
 
 export default function HomePage() {
   const dispatch = useDispatch();
@@ -15,10 +16,20 @@ export default function HomePage() {
   }, [dispatch]);
   return (
     <>
-      <div>
-        <h1>Welcome to Movie Quiz</h1>
-        <ImageList variant="quilted" cols={10} rowHeight={121}></ImageList>
-      </div>
+      <Container style={{ width: "100%" }}>
+        {/* <h1>Welcome to Movie Quiz</h1> */}
+        <ImageList variant="quilted" cols={4} rowHeight={195}>
+          {listOfMovies.map((item) => (
+            <ImageListItem key={item.id}>
+              <img
+                alt={item.title}
+                loading="lazy"
+                src={`${apiUrlPoster}${item.backdrop_path}`}
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Container>
     </>
   );
 }
