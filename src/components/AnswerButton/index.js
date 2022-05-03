9b179d0fc5f5ae952e2250a18bfd033e6d5de492
poojadalai <poojadalai@gmail.com>
2022-05-03 14:28:56 +0200

    style

--- src/components/AnswerButton/index.js
+++ src/components/AnswerButton/index.js
@@ -2,25 +2,46 @@ import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import { Button, Typography } from "@mui/material";
+import logo from "../../assests/logo.gif";
 
 export default function RowAndColumnSpacing(props) {
+  const [button, setButton] = React.useState(false);
   return (
     <Box sx={{ mt: 4, mb: 2 }}>
       <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         <Grid item xs={6}>
           <Button
-            // onClick={props.click}
+            onClick={() => {
+              setButton(true);
+            }}
             color="secondary"
             xs={6}
-            sx={{ p: 6, width: 1, borderRadius: "15px" }}
+            sx={{ width: 1, borderRadius: "15px" }}
             variant="contained"
             size="large"
           >
             <Typography
-              sx={{ fontFamily: `'Happy Monkey', cursive` }}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                fontFamily: `'Happy Monkey', cursive`,
+              }}
               variant="h5"
             >
-              {props.ans1}
+              <div sx={{ alignItems: "center", display: "flex" }}>
+                {props.ans1}
+              </div>
+              <div>
+                {button ? (
+                  <img
+                    sx={{ height: "100px!important" }}
+                    src={logo}
+                    alt="loading..."
+                  />
+                ) : (
+                  ""
+                )}
+              </div>
             </Typography>
           </Button>
         </Grid>
