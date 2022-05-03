651449c92993584bf26858c94176c1b2621dcc42
Alina Kovaleva <calipco.xp@gmail.com>
2022-05-03 14:25:36 +0200

    ScoreboardPage
        - init scoreboardPage
        - added store for scoreboard
        - added path for scoreboard (/scoreboard)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import YearQuiz from "./components/YearQuiz";
 import GamePage from "./pages/GamePage";
 import HomePage from "./pages/HomePage";
 import QuestionPage from "./pages/QuestionPage";
+import ScoreboardPage from "./pages/ScoreboardPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/question" element={<QuestionPage />} />
         <Route path="/year" element={<YearQuiz />} />
+        <Route path="/scoreboard" element={<ScoreboardPage />} />
       </Routes>
     </div>
   );
