fa1ae38c0e3b8f824466f3c75873b78bc50c91b1
poojadalai <poojadalai@gmail.com>
2022-05-04 13:51:37 +0200

    Added AnswerButton Feature

--- src/components/AnswerButton/index.js
+++ src/components/AnswerButton/index.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useState, useEffect, useRef } from "react";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import { Button, Typography } from "@mui/material";
@@ -21,223 +21,134 @@ import {
 } from "../../store/game/actions";
 
 export default function RowAndColumnSpacing() {
-  const [button, setButton] = React.useState(false);
   const questionNumber = useSelector(selectQuestionCounter);
   const shuffledQuestions = useSelector(selectShuffledQuestions);
   const dispatch = useDispatch();
-  const rightAnswers = useSelector(selectRightAnswers);
-  const round = useSelector(selectRoundProgress);
   const correctButton = useSelector(selectCorrectButton);
-  const [correct, setCorrect] = React.useState(4);
-  const [incorrect1, setIncorrect1] = React.useState(false);
-  const [incorrect2, setIncorrect2] = React.useState(false);
-  const [incorrect3, setIncorrect3] = React.useState(false);
-  const [incorrect4, setIncorrect4] = React.useState(false);
-  const delayedCounter = setTimeout(dispatch(incrementQuestionCounter), 3000);
 
-  // if(0 !== correctButton[round]) {setIncorrect1(true)} - after timeout setIncorrect1(false)
-  //   setCorrect(correctButton[round]) - after timeout setCorrect(4)
-  const handleAnswer1 = () => {
-    setCorrect(correctButton[round]);
-    if (0 === correctButton[round]) {
-      dispatch(incrementScore());
-      delayedCounter();
-    } else {
-      setIncorrect1(true);
-      delayedCounter();
-      setIncorrect1(false);
-    }
-    setCorrect(4);
+  const [timerState, setTimerState] = useState(null);
+  const [timePassed, setTimePassed] = useState(0);
+  const timeRef = useRef(timePassed);
+  timeRef.current = timePassed;
+
+  const [buttonsState, setButtonState] = useState([
+    { id: 0, correct: false },
+    { id: 1, correct: false },
+    { id: 2, correct: false },
+    { id: 3, correct: false },
+  ]);
+
+  // after user clicks => true
+  // when we load the next question => false
+  const [answered, setAnswered] = useState(false);
+
+  const createInterval = () => {
+    const timer = setInterval(() => {
+      setTimePassed(timeRef.current + 1);
+    }, 1000);
+
+    setTimerState(timer);
   };
 
-  const handleAnswer2 = () => {
-    setCorrect(correctButton[round]);
-    if (1 === correctButton[round]) {
-      dispatch(incrementScore());
-      delayedCounter();
-    } else {
-      setIncorrect2(true);
-      delayedCounter();
-      setIncorrect2(false);
+  useEffect(() => {
+    createInterval();
+  }, []);
+
+  useEffect(() => {
+    if (timeRef.current === 10) {
+      console.log("Reset timer! next question");
+      clearInterval(timerState);
+      updateButtonState();
+      // now time is up!
+      // we need to set a timeout to let users check out the answers
+
+      // setTimeout(() => {
+      //   dispatch(incrementQuestionCounter());
+      //   console.log("Delayed for 3 second.");
+      // }, "1000");
+
+      // when timeout is finished =>
+
+      // 1. Reset buttonState + answered.
+      setButtonState();
+      // 2. Load next question
+      // 3. Restart interval (10 secs)
     }
-    setCorrect(4);
+  }, [timeRef.current]);
+
+  // console.log("correct", correctButton);
+
+  const updateButtonState = () => {
+    const updatedButtonState = buttonsState.map((b) => ({
+      ...b,
+      correct: b.id === correctButton[questionNumber - 1],
+    }));
+    setButtonState(updatedButtonState);
+    setAnswered(true);
   };
 
-  const handleAnswer3 = () => {
-    setCorrect(correctButton[round]);
-    if (2 === correctButton[round]) {
-      dispatch(incrementScore());
-      delayedCounter();
-    } else {
-      setIncorrect3(true);
-      delayedCounter();
-      setIncorrect3(false);
-    }
-    setCorrect(4);
-  };
+  const handleClick = (buttonNr) => {
+    // know which option was selected
+    // check if it's the correct one
+    const isCorrectAnswer = correctButton[questionNumber - 1] === buttonNr; // [0-3]
+    // update all answers state + set answered to true
+    updateButtonState();
 
-  const handleAnswer4 = () => {
-    setCorrect(correctButton[round]);
-    if (3 === correctButton[round]) {
-      dispatch(incrementScore());
-      delayedCounter();
-    } else {
-      setIncorrect4(true);
-      delayedCounter();
-      setIncorrect4(false);
-    }
-    setCorrect(4);
+    // if correct answer => increase score
+    if (isCorrectAnswer) dispatch(incrementScore());
+    dispatch(incrementQuestionCounter());
   };
 
   return (
     <Box sx={{ mt: 4, mb: 2 }}>
+      <div>Timer: {10 - timePassed}</div>
       <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={6}>
-          <Button
-            onClick={() => {
-              setButton(true);
-              handleAnswer1();
-            }}
-            color="secondary"
-            xs={6}
-            sx={{ width: 1, height: "130px", borderRadius: "15px" }}
-            variant="contained"
-            size="large"
-          >
-            <Typography
-              className="main-button"
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                fontFamily: `'Happy Monkey', cursive`,
+        {[0, 1, 2, 3].map((i) => (
+          <Grid item xs={6}>
+            <Button
+              onClick={() => {
+                handleClick(i);
               }}
-              variant="h5"
+              color="secondary"
+              xs={6}
+              sx={{ width: 1, height: "130px", borderRadius: "15px" }}
+              variant="contained"
+              size="large"
             >
-              <div sx={{ alignItems: "center", display: "flex" }}>
-                {shuffledQuestions[questionNumber - 1][0]}
-              </div>
-              <div>
-                {button ? (
-                  <img
-                    className="icons"
-                    sx={{ height: "100px!important" }}
-                    src={rightIcon}
-                    alt="loading..."
-                  />
-                ) : (
-                  ""
-                )}
-              </div>
-            </Typography>
-          </Button>
-        </Grid>
-        <Grid item xs={6}>
-          <Button
-            onClick={() => {
-              setButton(true);
-              handleAnswer2();
-            }}
-            color="secondary"
-            xs={6}
-            sx={{ width: 1, height: "130px", borderRadius: "15px" }}
-            variant="contained"
-            size="large"
-          >
-            <Typography
-              className="main-button"
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                fontFamily: `'Happy Monkey', cursive`,
-              }}
-              variant="h5"
-            >
-              <div sx={{ alignItems: "center", display: "flex" }}>
-                {shuffledQuestions[questionNumber - 1][1]}
-              </div>
-              <div>
-                {button ? (
-                  <img className="icons" src={wrongIcon} alt="loading..." />
-                ) : (
-                  ""
-                )}
-              </div>
-            </Typography>
-          </Button>
-        </Grid>
-        <Grid item xs={6}>
-          <Button
-            onClick={() => {
-              setButton(true);
-              handleAnswer3();
-            }}
-            color="secondary"
-            xs={6}
-            sx={{ width: 1, height: "130px", borderRadius: "15px" }}
-            variant="contained"
-            size="large"
-          >
-            <Typography
-              className="main-button"
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                fontFamily: `'Happy Monkey', cursive`,
-              }}
-              variant="h5"
-            >
-              <div sx={{ alignItems: "center", display: "flex" }}>
-                {shuffledQuestions[questionNumber - 1][2]}
-              </div>
-              <div>
-                {button ? (
-                  <img
-                    className="icons"
-                    sx={{ height: "100px!important" }}
-                    src={wrongIcon}
-                    alt="loading..."
-                  />
-                ) : (
-                  ""
-                )}
-              </div>
-            </Typography>
-          </Button>
-        </Grid>
-        <Grid item xs={6}>
-          <Button
-            onClick={() => {
-              setButton(true);
-              handleAnswer4();
-            }}
-            color="secondary"
-            xs={6}
-            sx={{ width: 1, height: "130px", borderRadius: "15px" }}
-            variant="contained"
-            size="large"
-          >
-            <Typography
-              className="main-button"
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                fontFamily: `'Happy Monkey', cursive`,
-              }}
-              variant="h5"
-            >
-              <div sx={{ alignItems: "center", display: "flex" }}>
-                {shuffledQuestions[questionNumber - 1][3]}
-              </div>
-              <div>
-                {button ? (
-                  <img className="icons" src={wrongIcon} alt="loading..." />
-                ) : (
-                  ""
-                )}
-              </div>
-            </Typography>
-          </Button>
-        </Grid>
+              <Typography
+                className="main-button"
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  fontFamily: `'Happy Monkey', cursive`,
+                }}
+                variant="h5"
+              >
+                <div sx={{ alignItems: "center", display: "flex" }}>
+                  {shuffledQuestions[questionNumber - 1][i]}
+                </div>
+                <div>
+                  {answered &&
+                    (buttonsState[i].correct ? (
+                      <img
+                        className="icons"
+                        sx={{ height: "100px!important" }}
+                        src={rightIcon}
+                        alt="loading..."
+                      />
+                    ) : (
+                      <img
+                        className="icons"
+                        sx={{ height: "100px!important" }}
+                        src={wrongIcon}
+                        alt="loading..."
+                      />
+                    ))}
+                </div>
+              </Typography>
+            </Button>
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
