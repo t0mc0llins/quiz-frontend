7443b8846138f4c0867a8dfafbcc3b530c0671fe
Vasileios1314 <vasilis.bobonis@hotmail.com>
2022-05-04 15:21:19 +0200

    complete wheel

--- src/store/gamePage/reducer.js
+++ src/store/gamePage/reducer.js
@@ -2,53 +2,7 @@ import { FETCH_QUESTION, FETCH_USER_ANSWER } from "./actions";
 import { FETCH_CATEGORIES } from "./types";
 
 const initialState = {
-  categories: ["Comedy", "Drama", "Horror", "Action", "Western"],
-
-  // questions: {
-  //   id: 2,
-  //   question:
-  //     "What does Annie get a job as in the 2007 movie The Nanny Diaries?",
-  //   hint: null,
-  //   createdAt: "2022-05-02T10:15:23.071Z",
-  //   updatedAt: "2022-05-02T10:15:23.071Z",
-  //   categoryId: 1,
-  //   answers: [
-  //     {
-  //       id: 5,
-  //       answer: "A Nanny",
-  //       correct: true,
-  //       createdAt: "2022-05-02T10:15:23.167Z",
-  //       updatedAt: "2022-05-02T10:15:23.167Z",
-  //       questionId: 2,
-  //     },
-  //     {
-  //       id: 6,
-  //       answer: "Elijah",
-  //       correct: false,
-  //       createdAt: "2022-05-02T10:15:23.167Z",
-  //       updatedAt: "2022-05-02T10:15:23.167Z",
-  //       questionId: 2,
-  //     },
-  //     {
-  //       id: 7,
-  //       answer: "Charlotte",
-  //       correct: false,
-  //       createdAt: "2022-05-02T10:15:23.167Z",
-  //       updatedAt: "2022-05-02T10:15:23.167Z",
-  //       questionId: 2,
-  //     },
-  //     {
-  //       id: 8,
-  //       answer: "Ava",
-  //       correct: false,
-  //       createdAt: "2022-05-02T10:15:23.167Z",
-  //       updatedAt: "2022-05-02T10:15:23.167Z",
-  //       questionId: 2,
-  //     },
-  //   ],
-  // },
-  // userAnswer: [],
-  // categories: [],
+  categories: ["Actor", "Year", "Director", "Oddone out"],
 };
 
 export default function gamePageReducer(state = initialState, action) {
@@ -69,3 +23,49 @@ export default function gamePageReducer(state = initialState, action) {
       return state;
   }
 }
+
+// questions: {
+//   id: 2,
+//   question:
+//     "What does Annie get a job as in the 2007 movie The Nanny Diaries?",
+//   hint: null,
+//   createdAt: "2022-05-02T10:15:23.071Z",
+//   updatedAt: "2022-05-02T10:15:23.071Z",
+//   categoryId: 1,
+//   answers: [
+//     {
+//       id: 5,
+//       answer: "A Nanny",
+//       correct: true,
+//       createdAt: "2022-05-02T10:15:23.167Z",
+//       updatedAt: "2022-05-02T10:15:23.167Z",
+//       questionId: 2,
+//     },
+//     {
+//       id: 6,
+//       answer: "Elijah",
+//       correct: false,
+//       createdAt: "2022-05-02T10:15:23.167Z",
+//       updatedAt: "2022-05-02T10:15:23.167Z",
+//       questionId: 2,
+//     },
+//     {
+//       id: 7,
+//       answer: "Charlotte",
+//       correct: false,
+//       createdAt: "2022-05-02T10:15:23.167Z",
+//       updatedAt: "2022-05-02T10:15:23.167Z",
+//       questionId: 2,
+//     },
+//     {
+//       id: 8,
+//       answer: "Ava",
+//       correct: false,
+//       createdAt: "2022-05-02T10:15:23.167Z",
+//       updatedAt: "2022-05-02T10:15:23.167Z",
+//       questionId: 2,
+//     },
+//   ],
+// },
+// userAnswer: [],
+// categories: [],
