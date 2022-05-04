b89589869223060552810f727fb624327d3dbc46
t0mc0llins <tomcollins93@gmail.com>
2022-05-04 09:58:00 +0200

    working on answer animations

--- src/store/game/selectors.js
+++ src/store/game/selectors.js
@@ -1,2 +1,11 @@
 export const selectQuestionCounter = (reduxState) => reduxState.game.counter;
 export const selectScore = (reduxState) => reduxState.game.score;
+export const selectCounterOffset = (reduxState) => {
+  if (reduxState.game.counter - 4 <= 0) {
+    return 0;
+  } else if (reduxState.game.counter - 8 <= 0) {
+    return 4;
+  } else {
+    return 8;
+  }
+};
